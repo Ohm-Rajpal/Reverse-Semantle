73b8621fd81f27248257c1718de221d48879d518
Ohm-Rajpal <137588636+Ohm-Rajpal@users.noreply.github.com>
2024-07-27 21:25:47 -0700

    Added second input for similarity rating

--- reverse-semantle/app/page.tsx
+++ reverse-semantle/app/page.tsx
@@ -7,11 +7,16 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col">
 			<h3>Enter a Word</h3>
-			<p>This is just a demo</p>
 			<form className="m-3 p-3 bg-slate-100 rounded-lg">
-				<input className="m-3 p-3 border-2 border-slate-200 rounded-lg w-fit" placeholder="Meow" type="text" content={formInput} onChange={ event => {
+				<input className="m-3 p-3 border-2 border-slate-200 rounded-lg w-fit" placeholder="Enter Word"
+					   type="text" content={formInput} onChange={event => {
 					setFormInput(event.target.value);
-				} }/>
+				}}/>
+				<input
+					className="m-3 p-3 border-2 border-slate-200 rounded-lg w-fit"
+					placeholder="Similarity Rating" type="text" content={formInput}
+					onChange={event => { setFormInput(event.target.value);
+				}}/>
 				<button type={"submit"} className="m-3 p-3 bg-blue-500 text-white rounded-lg">Submit</button>
 			</form>
 		</main>
